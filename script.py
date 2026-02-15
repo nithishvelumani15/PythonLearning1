d7d6e17f4e967fc9bfbcf78e3d1c6e0f2b98e769
nithishvelumani15 <nithishvelumani15@gmail.com>
2026-02-15 12:26:12 +0530

    script.py updated by Nithish

--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-print("Hi")
+class Dog:
+    def __init__(self,name,breed,owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
+    def bark(self):
+        print("Whoof Whoof")
+    def greeting(self):
+        print(f"I am {self.owner.name} and {self.name} is my dog")
+class Owner:
+    def __init__(self,name,address,contact,lst):
+        self.name = name
+        self.address = address
+        self.contact = contact
+        self.lst = lst
+    def arraySort(self):
+        return sorted(self.lst)
+
+
+owner1 = Owner("Nithish","India","00-000",[5,1,5,3,5,6,7,8])
+dog1 = Dog("Tom","BullDog",owner1)
+dog1.greeting()
+print("Sorted Array is: ",owner1.arraySort())
