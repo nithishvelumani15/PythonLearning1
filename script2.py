f99c470f0930ad153a2bec0c0bf0297e3ba4bae7
nithishvelumani15 <nithishvelumani15@gmail.com>
2026-02-15 22:10:52 +0530

    sscript2.py modified

--- script2.py
+++ script2.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 class User:
     def __init__(self,name,email,password):
         self.name = name
@@ -14,10 +13,22 @@ class User:
     def set_email(self,new_email):
         print(f"Email updated at {datetime.now()}")
         self._email = new_email
+    @property
+    def email(self):
+        print(f"Email accessed at {datetime.now()}")
+        return self._email
+    
+    @email.setter
+    def email(self,new_email):
+        print(f"Email updated at {datetime.now()}")
+        self._email = new_email
 
 user1 = User("Nithish","  TESt1@email.com ",1234)
 user2 = User("Dhivyasri","test2@gmail.com",4234)
-print("Before",user1.get_email())
+'''print("Before",user1.get_email())
 user1.set_email("nithish@gmail.com")
 print("After",user1.get_email())
-
+'''
+print(user1.email)
+user1.email = "nithish@gmail.com"
+print(user1.email)
